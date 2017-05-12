5d86b0edb90e0a0f6e8c8a59868e7886796d3bc6
Peter <goroh83@yahoo.com>
2017-05-12 10:47:26 +0200

    Intra investment initial repo

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+
+
+$(function () {
+
+    setTimeout(function () {
+        $('.fly-in-text').removeClass('hidden');
+    }, 500);
+
+
+});
+
+$(function (){
+
+$(".banner-text").fadeIn("slow");
+
+})
+
+
+
+$(function () {
+
+    $('.menu-img').on('click', function () {
+        $(".menu-list").fadeToggle("slow");
+    });
+
+});
+
+
+/*
+    $('.menu-img').hover( function () {
+        $(".menu-list").fadeToggle("slow");
+    });
+
+
+    */
